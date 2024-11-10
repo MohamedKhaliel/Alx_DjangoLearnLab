2011c0e08b2b080a3989baa5f2643b9e34446961
MohamedKhaliel <mohamedahmed201131@gmail.com>
2024-11-10 23:16:39 +0200

    edit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,11 +6,14 @@ from django.contrib.auth.views import LogoutView
 from .views import register
 from . import views
 
+
 urlpatterns = [
     path('library/', LibraryDetailView.as_view(), name='library-detail'),
     path('list_books/', list_books, name='list_books'),
-    path('register/', views.register.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    
+    path('register/', views.register.as_view(), name='register'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from .models import Library
+from .models import Book 
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
-from .models import Library
-from .models import Book 
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
 
 def list_books(request):
     books = Book.objects.all() 
@@ -21,8 +23,33 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)  
         context['book'] = self.object.books.all()
         return context
-
+    
 class register(CreateView):
-    form_class = UserCreationForm() 
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+# Check if the user has the 'Admin' role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    # This view is only accessible to users with the 'Admin' role
+    return render(request, 'relationship_app/admin_view.html')  # You can customize the template as needed
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    # This view is only accessible to users with the 'Librarian' role
+    return render(request, 'relationship_app/librarian_view.html')  # Customize the template as needed
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    # This view is only accessible to users with the 'Member' role
+    return render(request, 'relationship_app/member_view.html')  # Customize the template as needed
