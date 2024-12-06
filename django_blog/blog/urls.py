2b60ab6fd067164d19c60d91c394735dba79d6cb
mohamed khalil <mohamedahmed201131@gmail.com>
2024-12-06 22:08:16 +0200

    add

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view() , name='create_comment'),
     path('post_list/<int:post_id>/', DetailView.as_view(), name='post_detail'),
     path('posts/<int:post_id>/comments/', views.CommentListView.as_view() , name='comment_list'),
-    path ('posts/<int:post_id>/comments/<int:pk>/edit', views.CommentUpdateView.as_view() , name='comment_update'),
+    path ('posts/<int:post_id>/comments/<int:pk>/update', views.CommentUpdateView.as_view() , name='comment_update'),
     path ('posts/<int:post_id>/comments/<int:pk>/delete', views.CommentDeleteView.as_view() , name='comment_delete'),
 ]
