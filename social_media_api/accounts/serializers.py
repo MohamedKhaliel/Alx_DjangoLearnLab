6c454f8a1a48edc3ec5a2329a983e52c3b7d39c2
mohamed khalil <mohamedahmed201131@gmail.com>
2024-12-13 19:47:54 +0200

    add

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,13 +10,28 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'},  # Ensures the password input is masked in forms
+    )
+    confirm_password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'},
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'password', 'confirm_password', 'email']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return attrs
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')  # Remove confirm_password as it's not needed in the model
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
